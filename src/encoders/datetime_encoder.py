de75e727293a76bc8d2804e86b6e61a888372453
Vlad <predgd@pm.me>
2022-09-24 14:43:06 +0200

    delete encoder class

--- src/encoders/datetime_encoder.py
+++
@@ -1,8 +0,0 @@
-import datetime
-from json import JSONEncoder
-
-
-class DateTimeEncoder(JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, (datetime.date, datetime.datetime)):
-            return obj.isoformat()
