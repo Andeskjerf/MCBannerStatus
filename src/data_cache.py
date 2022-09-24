887e1fb1e95246a31f7ae7b3a3ccbe1153ebfd90
Vlad <predgd@pm.me>
2022-09-24 14:03:08 +0200

    add last update time to cache

--- src/data_cache.py
+++ src/data_cache.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 
 
@@ -7,12 +8,21 @@ class DataCache:
     online_players: int = 0
     version: str = None
     active: bool = False
+    last_update: datetime = None
 
-    def __init__(self, max_players, online_players, version, active):
+    def __init__(
+        self,
+        max_players,
+        online_players,
+        version,
+        active,
+        last_update=None
+    ):
         self.max_players = max_players
         self.online_players = online_players
         self.version = version
         self.active = active
+        self.last_update = last_update
 
     def to_json(self):
         return json.dumps(self, default=lambda o: o.__dict__)
